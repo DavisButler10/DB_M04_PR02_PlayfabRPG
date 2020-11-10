b3143c295a9440d0cc20ee82de9ed3c3184d152a
DavisButler10 <davis.d.butler@gmail.com>
2020-11-10 09:58:12 -0700

    Everything

---
+++ Scripts/Leaderboard.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+
+public class Leaderboard : MonoBehaviour
+{
+    public GameObject leaderboardCanvas;
+    public GameObject[] leaderboardEntries;
+
+    public static Leaderboard instance;
+    void Awake() { instance = this; }
+
+    public void OnEndGame()
+    {
+        leaderboardCanvas.SetActive(true);
+        SetLeaderboardEntry(PlayerController.me.gold, 10);
+        DisplayLeaderboard();
+    }
+
+    public void DisplayLeaderboard()
+    {
+        GetLeaderboardRequest getLeaderboardRequest = new GetLeaderboardRequest
+        {
+            StatisticName = "Gold"
+        };
+
+        PlayFabClientAPI.GetLeaderboard(getLeaderboardRequest,
+            result => UpdateLeaderboardUI(result.Leaderboard),
+            error => Debug.Log(error.ErrorMessage)
+        );
+    }
+
+    void UpdateLeaderboardUI(List<PlayerLeaderboardEntry> leaderboard)
+    {
+        for (int x = 0; x < leaderboardEntries.Length; x++)
+        {
+            leaderboardEntries[x].SetActive(x < leaderboard.Count); //x < leaderboard.Count
+            if (x >= leaderboard.Count) continue;
+            leaderboardEntries[x].transform.Find("PlayerName").GetComponent<TextMeshProUGUI>().text = (leaderboard[x].Position + 1) + ". " + leaderboard[x].DisplayName;
+            leaderboardEntries[x].transform.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = ((float)leaderboard[x].StatValue).ToString() + " gold";
+        }
+
+    }
+
+    public void SetLeaderboardEntry(int gold, int kills)
+    {
+        Debug.Log(gold);
+        ExecuteCloudScriptRequest request = new ExecuteCloudScriptRequest
+        {
+            FunctionName = "UpdateGold",
+            FunctionParameter = new { score = gold }
+        };
+        PlayFabClientAPI.ExecuteCloudScript(request,
+         result => DisplayLeaderboard(),
+         error => Debug.Log(error.ErrorMessage)
+        );
+    }
+}
